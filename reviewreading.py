0f6c074e9ea1c8df19ce9628a3bb796a97360872
iammidhun <midhunmohanputhur@gmail.com>
2019-12-25 16:40:42 +0530

    tets  data set reading

--- reviewreading.py
+++ reviewreading.py
@@ -5,3 +5,8 @@ with  open('../aclImdb/movie_data/full_train.txt') as ifile:
         train_data.append(i.strip())
         
 
+test_data  = []
+
+with open('../aclImdb/movie_data/full_test.txt') as ifile:
+	for i in ifile:
+		test_data.append(i.strip())
