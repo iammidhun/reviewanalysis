8453a25e379531111a3b05bedd4d11edbfb4999d
iammidhun <midhunmohanputhur@gmail.com>
2019-12-25 19:01:19 +0530

    pylint

--- reviewreading.py
+++ reviewreading.py
@@ -1,12 +1,10 @@
 train_data = []
+test_data  = []
 
 with  open('../aclImdb/movie_data/full_train.txt') as ifile:
     for i in ifile:
         train_data.append(i.strip())
         
-
-test_data  = []
-
 with open('../aclImdb/movie_data/full_test.txt') as ifile:
 	for i in ifile:
 		test_data.append(i.strip())
